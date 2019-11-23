200ba0fe0fc081e4f69f6c322ff920d320c86aa4
Carlos Arbildo <carlos.arbildo@orbis.com.pe>
2019-11-23 13:15:46 -0500

    auth refactor

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -7,60 +7,30 @@ use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\EventListener\ValidateRequestListener;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * @Route("/auth", name="auth")
  */
 class AuthController extends AbstractController
 {
-    private $jwtEncoder;
-
-    public function __construct(JWTEncoderInterface $JWTEncoder)
-    {
-        $this->jwtEncoder = $JWTEncoder;
-    }
+    const IGNORED_VALUES = ['ignored_attributes' => ['__initializer__','__cloner__','__isInitialized__', 'usuario', 'password']];
 
     /**
      * @Route("/login", name="login")
      */
-    public function index(Request $request)
+    public function login(Request $request, SerializerInterface $serializer)
     {
         $request = $request->getContent();
         $data = json_decode($request,true);
-
         $em             = $this->getDoctrine()->getManager();
         $usuarioRepository    = $em->getRepository(TblUsuario::class);
         $userInfo = $usuarioRepository->findOneBy($data);
-
-        $data = $this->serializeUser($userInfo);
-
-        $encoded = $this->jwtEncoder->encode($data);
-
-        $response = new JsonResponse(["token" => $encoded], 200);
-
+        $result = $serializer->serialize($userInfo, 'json', self::IGNORED_VALUES);
+        $response = new Response($result, 200,['Content-Type'=> 'application/json'] );
         return $response;
-    }
-
-    private function serializeUser($data)
-    {
-        if (empty($data)){
-            return ['data' => []];
-        }
-        return [
-            'data'=> [
-            'nombres' =>$data->getNombres(),
-            'apellidos' =>$data->getApellidos(),
-            'usuario' =>$data->getUsuario(),
-            'correo' =>$data->getCorreo(),
-            'foto' =>$data->getFoto(),
-            'cargo' => [
-                'id'=>$data->getCargo()->getIdCargo(),
-                'nombre'=>$data->getCargo()->getNombre(),
-                'estado'=>$data->getCargo()->getEstado(),
-                ]
-        ]];
-
     }
 }
